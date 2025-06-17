adabb03441f07d91e732a3c4aba65cae10c46c9c
PSujeet29 <sujeetpandey29061@gmail.com>
2025-06-17 14:50:03 +0545

    managing first components in the baaasic react

---
+++ 01basicreact/src/Football.js
@@ -0,0 +1,9 @@
+function footbal() {
+    return (
+    <>
+        <h2>my favourite club</h2>
+        <p>Helllo, i love watchinf football. my favourite club is manchester united. it came 15th in the premierleague season 24/25. I hope manchester united sign viktor gyokeeres and brian mbeumo and emi martinez and some good defender and midfielder. we already got cunha. i hope manchester united to get bacak ijto top4, to the place where they truly belong to.</p>
+    </>
+    )
+}
+export default footbal
--- 01basicreact/src/index.js
+++ 01basicreact/src/index.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import Footbal from './Football';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
+    <Footbal/>
   </React.StrictMode>
 );
 
